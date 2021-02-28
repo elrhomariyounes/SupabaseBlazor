7db9def128d9ce2b8d848857187c05115f79c8f6
elrhomari.younes <elrhomari.younes@gmail.com>
2021-02-28 05:57:27 +0100

    Add project files.

---
+++ SupabaseBlazor/Services/AuthenticationService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+using Supabase.Gotrue;
+using System;
+using System.Threading.Tasks;
+namespace SupabaseBlazor.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly Supabase.Client _supabaseClient;
+        private readonly NavigationManager _navigationManager;
+        public AuthenticationService(NavigationManager navigationManager)
+        {
+            _navigationManager = navigationManager;
+
+            _supabaseClient = Supabase.Client.Initialize(
+                Environment.GetEnvironmentVariable("supabaseUrl"),
+                Environment.GetEnvironmentVariable("supabaseKey"));
+        }
+
+        public User User { get; private set; }
+
+        public async Task Login(string email, string password)
+        {
+            var user = await _supabaseClient.Auth.SignUp(email, password);
+            User = user.User;
+        }
+
+        public async Task Logout()
+        {
+            User = null;
+            await _supabaseClient.Auth.SignOut();
+            _navigationManager.NavigateTo("/");
+        }
+    }
+}
---
+++ SupabaseBlazor/Services/IAuthenticationService.cs
@@ -0,0 +1,12 @@
+﻿using Supabase.Gotrue;
+using System.Threading.Tasks;
+
+namespace SupabaseBlazor.Services
+{
+    public interface IAuthenticationService
+    {
+        User User { get; }
+        Task Login(string email, string password);
+        Task Logout();
+    }
+}
